185fc8e80160d2f05d9a62e05a8ee9ebb11e7586
JiriVALASEK <jirka362@gmail.com>
2019-05-30 15:51:19 +0200

    InitGui rewritten from template to be working
    Class was rewritten to be working with PythonFeature Classes, which were renamed.

--- InitGui.py
+++ InitGui.py
@@ -4,51 +4,51 @@ Created on Mon Apr 22 23:32:41 2019
 
 @author: jirka
 """
-import FreeCAD, FreeCADGui, Workbench
-from os import path
+import FreeCAD, FreeCADGui
 
 class Animate(Workbench):
-    "Animate workbench object"
+	"Animate workbench object"
 	
-    def __init__(self):
-        self.__class__.Icon = path.join (FreeCAD.getResourceDir(), "Mod",
-										 "Animate","Resources","Icons",
-										 "Animate.xpm")
-        self.__class__.MenuText = "Animate"
-        self.__class__.ToolTip = "Animation workbench"
-    
+	def __init__(self):
+		import os
+		self.__class__.Icon = os.path.join(FreeCAD.getHomePath(),"Mod",
+										   "Animate","Resources","Icons",
+										   "Animate.xpm")
+		self.__class__.MenuText = "Animate"
+		self.__class__.ToolTip = "Animation workbench"
+	
 
-    def Initialize(self):
-        "This function is executed when FreeCAD starts"
+	def Initialize(self):
+		"This function is executed when FreeCAD starts"
 		# import here all the needed files that create your FreeCAD commands
-        import AnimationServer, AnimationControl, AnimationTrajectory
+		import Server, Control, Trajectory
 		# A list of command names created in the line above
-        self.list = ["AnimationServerCommand","AnimationControlCommand",
-					 "AnimationTrajectoryCommand"]
+		self.list = ["ServerCommand", "ControlCommand", "TrajectoryCommand"]
 		# creates a new toolbar with your commands
-        self.appendToolbar("Animation Commands",self.list)
+		self.appendToolbar("Animation Commands",self.list)
 		# creates a new menu
-        self.appendMenu("AnimationCommands",self.list)
+		self.appendMenu("AnimationCommands",self.list)
 		# appends a submenu to an existing menu
-        #self.appendMenu(["An existing Menu","My submenu"],self.list)
+		#self.appendMenu(["An existing Menu","My submenu"],self.list)
 
-    def Activated(self):
-        "This function is executed when the workbench is activated"
-        FreeCAD.Console.PrintMessage("Animation workbench activated\n")
-        return
+	def Activated(self):
+		"This function is executed when the workbench is activated"
+		FreeCAD.Console.PrintMessage("Animate workbench activated\n")
+		return
 
-    def Deactivated(self):
-        "This function is executed when the workbench is deactivated"
-        FreeCAD.Console.PrintMessage("Animation workbench deactivated\n")
-        return
+	def Deactivated(self):
+		"This function is executed when the workbench is deactivated"
+		FreeCAD.Console.PrintMessage("Animate workbench deactivated\n")
+		return
 
-    def ContextMenu(self, recipient):
-        "This is executed whenever the user right-clicks on screen"
-        # "recipient" will be either "view" or "tree"
-        self.appendContextMenu("My commands",self.list) # add commands to the context menu
+	def ContextMenu(self, recipient):
+		"This is executed whenever the user right-clicks on screen"
+		# "recipient" will be either "view" or "tree"
+		#self.appendContextMenu("My commands",self.list) # add commands to the context menu
+		pass
 
-    def GetClassName(self): 
-        # this function is mandatory if this is a full python workbench
-        return "Gui::PythonWorkbench"
-       
+	def GetClassName(self): 
+		# this function is mandatory if this is a full python workbench
+		return "Gui::PythonWorkbench"
+	   
 FreeCADGui.addWorkbench(Animate())
