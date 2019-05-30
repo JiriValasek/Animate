741cbdaf4cde6aeff4165e8cacca5923be1bff21
JiriVALASEK <jirka362@gmail.com>
2019-05-30 15:42:55 +0200

    AnimationControl filename shortened
    Prefix Animation was deleted to make names shorter and without irrelevant information.

--- Control.py
+++ Control.py
@@ -5,9 +5,14 @@ Created on Tue May 21 23:23:30 2019
 @author: jirka
 """
 
-import FreeCAD
+import FreeCAD, FreeCADGui
 
-class AnimationControl: 
+from os import path
+
+_PATH_ICONS = path.join(FreeCAD.getHomePath(),"Mod","Animate","Resources",
+						"Icons")
+
+class Control: 
 
 	updated = False
 	
@@ -34,7 +39,7 @@ class AnimationControl:
 		
 		Parameters
 		----------
-		fp : Part::FeaturePython AnimationTrajectory object
+		fp : Part::FeaturePython Trajectory object
 			`fp` is an object which property has changed.
 		prop : String
 			`prop` is a name of a changed property.
@@ -71,22 +76,71 @@ class AnimationControl:
 		
 		Parameters
 		----------
-		fp : Part::FeaturePython AnimationTrajectory object
+		fp : Part::FeaturePython Trajectory object
 			`fp` is an object which property has changed.
 		"""
 		pass
 
-class ViewProviderAnimationControl:
+class ViewProviderControl:
 	
 	def __init__(self, obj):
 		''' Set this object to the proxy object of the actual view provider '''
+		obj.setEditorMode("AngularDeflection", 2)
+		obj.setEditorMode("BoundingBox", 2)
+		obj.setEditorMode("Deviation", 2)
+		obj.setEditorMode("DisplayMode", 2)
+		obj.setEditorMode("DrawStyle", 2)
+		obj.setEditorMode("Lighting", 2)
+		obj.setEditorMode("LineColor", 2)
+		obj.setEditorMode("LineWidth", 2)
+		obj.setEditorMode("PointColor", 2)
+		obj.setEditorMode("PointSize", 2)
+		obj.setEditorMode("Selectable", 2)
+		obj.setEditorMode("SelectionStyle", 2)
+		obj.setEditorMode("ShapeColor", 2)
+		obj.setEditorMode("Transparency", 2)
+		obj.setEditorMode("Visibility", 2)
 		obj.Proxy = self
 
 	def getDefaultDisplayMode(self):
 		''' Return the name of the default display mode. It must be defined in getDisplayModes. '''
-		return "Flat Lines"
+		return None
+	
+	def getIcon(self):
+		""" 
+		getIcon(self)
+		
+		Get the icon in XMP format which will appear in the tree view.
+		"""
+		return path.join(_PATH_ICONS, "Control.xpm")
 
-a=FreeCAD.ActiveDocument.addObject("Part::FeaturePython","AnimationControl")
-AnimationControl(a)
-ViewProviderAnimationControl(a.ViewObject)
-App.ActiveDocument.recompute()
+
+class ControlCommand(object):
+	"""Create Object command"""
+
+	def GetResources(self):
+		return {'Pixmap'  : path.join(_PATH_ICONS, "ControlCmd.xpm") ,
+            'MenuText': "Control" ,
+            'ToolTip' : "Create Control instance."}
+ 
+	def Activated(self):
+		doc = FreeCAD.ActiveDocument
+		a=doc.addObject("Part::FeaturePython","Control")
+		Control(a)
+		if FreeCAD.GuiUp:
+			ViewProviderControl(a.ViewObject)
+		doc.recompute()
+		return
+   
+	def IsActive(self):
+		if FreeCAD.ActiveDocument == None:
+			return False
+		else:
+			return True
+
+	def getHelp(self):
+		return ["This is help for  Control\n",
+				"and it needs to be written."]
+		
+if FreeCAD.GuiUp:
+	FreeCADGui.addCommand('ControlCommand', ControlCommand())
