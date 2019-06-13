e53bf1fef2d0aa1e8e823dbf741252cb7a71fc16
JiriVALASEK <jirka362@gmail.com>
2019-06-13 13:09:31 +0200

    Workbench import correction, commented unused commands in Init
    Animate class was inheriting from Workbench which wasn't imported from FreeCADGui. Example commands in Init.py were commented.

--- Init.py
+++ Init.py
@@ -21,7 +21,7 @@
 #*                                                                         *
 #***************************************************************************/
 
-FreeCAD.addImportType("My own format (*.own)","importOwn")
-FreeCAD.addExportType("My own format (*.own)","importOwn")
-print "I am executing some stuff here when FreeCAD starts!"
+# FreeCAD.addImportType("My own format (*.own)","importOwn")
+# FreeCAD.addExportType("My own format (*.own)","importOwn")
+# print("I am executing some stuff here when FreeCAD starts!")
 
