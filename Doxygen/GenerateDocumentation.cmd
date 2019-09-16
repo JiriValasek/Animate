5fb8578d7986597c4196b00a7882046f11fbd34a
JiriVALASEK <jirka362@gmail.com>
2019-09-17 01:36:58 +0200

    Revert "Revert "Updated website 4""
    This reverts commit 1d99a00002ef7731557b89d95c12b63d041b0c97.

--- Doxygen/GenerateDocumentation.cmd
+++ Doxygen/GenerateDocumentation.cmd
@@ -5,7 +5,7 @@ SET "scripts_directory=%CD%"
 SET "output_directory=%CD%\Doxypypy outputs"
 SET "doxygen_file=.\Doxyfile"
 :: remove resize.js from this directory
-SET "doxygen_output_directory=..\docs\programmers_doxy_doc"
+SET "doxygen_output_directory=..\Help\Documentation"
 
 :: Sort out input arguments
 SET output_dir_set=0
